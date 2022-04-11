e0bcdedc05ceef5dd8522abf7abfa72652a9f732
KimHS0915 <remember2552@gmail.com>
2022-04-12 03:24:26 +0900

    GraphQL mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,4 +1,4 @@
-export const books = [
+export let books = [
   {
     id: 0,
     name: "C Programming Language",
@@ -31,9 +31,32 @@ export const books = [
   },
 ];
 
+let newId = books.length;
+
 export const getBooks = () => books;
 
 export const getById = (id) => {
   const filtered = books.filter((book) => id === book.id);
   return filtered[0];
+};
+
+export const deleteBook = (id) => {
+  const cleanedBooks = books.filter((book) => book.id !== id);
+  if (books.length > cleanedBooks.length) {
+    books = cleanedBooks;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addBook = (name, author, score) => {
+  const newBook = {
+    id: newId++,
+    name,
+    author,
+    score,
+  };
+  books.push(newBook);
+  return newBook;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,13 @@
-import { getBooks, getById } from "./db";
+import { getBooks, getById, addBook, deleteBook } from "./db";
 
 const resolvers = {
   Query: {
     books: () => getBooks(),
     book: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addBook: (_, { name, author, score }) => addBook(name, author, score),
+    deleteBook: (_, { id }) => deleteBook(id),
   },
 };
 
