a9b5a8dd3a25833f4f95d3546f166bce050f066a
KimHS0915 <remember2552@gmail.com>
2022-04-14 02:58:25 +0900

    Update API

--- movie/graphql/db.js
+++ movie/graphql/db.js
@@ -1,16 +1,46 @@
 import { fetch } from "cross-fetch";
 
-const API_URL = process.env.API_URL;
+const LIST_URL = process.env.LIST_URL;
+const DETAIL_URL = process.env.DETAIL_URL;
+const SUGGESTIONS_URL = process.env.SUGGESTIONS_URL;
 
-export const getMovies = (limit, rating) => {
-  let REQUEST_URL = API_URL;
+const getRequestUrl = (url) => {
+  return `${url}?`;
+};
+
+const getResponseJSON = (url) => {
+  return fetch(`${url}`).then((response) => response.json());
+};
+
+export const getMovies = (limit, page, rating, genre) => {
+  let REQUEST_URL = getRequestUrl(LIST_URL);
   if (limit > 0) {
     REQUEST_URL += `limit=${limit}`;
+  }
+  if (page > 0) {
+    REQUEST_URL += `&page=${page}`;
   }
   if (rating > 0) {
     REQUEST_URL += `&minimum_rating=${rating}`;
   }
-  return fetch(`${REQUEST_URL}`)
-    .then((response) => response.json())
-    .then((json) => json.data.movies);
+  if (genre) {
+    REQUEST_URL += `&genre=${genre}`;
+  }
+  return getResponseJSON(REQUEST_URL).then((json) => json.data.movies);
+};
+
+export const getMovie = (id) => {
+  let REQUEST_URL = getRequestUrl(DETAIL_URL);
+  if (id > 0) {
+    REQUEST_URL += `movie_id=${id}`;
+  }
+  return getResponseJSON(REQUEST_URL).then((json) => json.data.movie);
+};
+
+export const getSuggestions = (id) => {
+  let REQUEST_URL = getRequestUrl(SUGGESTIONS_URL);
+  if (id > 0) {
+    REQUEST_URL += `movie_id=${id}`;
+  }
+  return getResponseJSON(REQUEST_URL).then((json) => json.data.movies);
 };
--- movie/graphql/resolvers.js
+++ movie/graphql/resolvers.js
@@ -1,8 +1,11 @@
-import { getMovies } from "./db";
+import { getMovie, getMovies, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
-    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movies: (_, { limit, page, rating, genre }) =>
+      getMovies(limit, page, rating, genre),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
 };
 
